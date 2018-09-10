4d29e5e94093fe0bd6a03eab0b5ef71ea72a6261
Vijendra Yadav <vijendra@vijendras-macbook-pro.local>
2018-09-10 08:05:16 +0530

    clean code

--- MVVM Example/ViewController.swift
+++ MVVM Example/ViewController.swift
@@ -13,38 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var txtMobileNo: UITextField!
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func loginPressed(_ sender: Any)
     {
         if (txtMobileNo.text?.count)! > 0 && (txtPassword.text?.count)! > 0
         {
-            var viewModel_ = LoginViewModel(mobileNo: txtMobileNo.text!, password: txtPassword.text!)
-            
+            let viewModel_ = LoginViewModel(mobileNo: txtMobileNo.text!, password: txtPassword.text!)
             
             viewModel_.logindemo { (isTrue) in
                 print("login response \(isTrue)")
             }
-            
-            
-            
         }
         else
-        {
-            
-        }
+        {}
     }
     
 }
-
